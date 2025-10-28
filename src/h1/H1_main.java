9c95b4b5b35893b22b506ef09ad5e5be6151ae5a
Angela Klenner <angela@Mac.fritz.box>
2025-10-28 22:39:01 +0100

    Hausaufgabe 1: Bedingte Subtraktion hinzugefügt

---
+++ src/h1/H1_main.java
@@ -0,0 +1,15 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+
+		int z = 22; // Beispielwert
+
+		if (z > 10) { // Wenn z größer als 10 ist:
+			z = z - 10; // Wenn ja, dann 10 abziehen
+		}
+
+		System.out.println("z = " + z); // "Zeig mir den Wert von z"
+	}
+}
