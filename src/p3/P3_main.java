2e7b22939041057db0f7be3b73702c9f3bfe632f
Angela Klenner <angela@Mac.fritz.box>
2025-10-28 22:23:54 +0100

    Präsenzaufgabe 3: Sinnvolle Testfälle

---
+++ src/p3/P3_main.java
@@ -0,0 +1,26 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+
+		int i = 2; // Beispielwerte
+		int j = 4;// Beispielwerte
+		int k = 10;// Beispielwerte
+
+		// Prüfen, ob i kleiner als j ist
+		if (i < j) {
+			System.out.println("A");
+		}
+		// Prüfen, ob i kleiner als k ist
+		if (i < k) {
+			System.out.println("B");
+
+		}
+		// Prüfen, ob j kleiner als k ist
+		if (j < k) {
+			System.out.println("C");
+
+		}
+	}
+}
